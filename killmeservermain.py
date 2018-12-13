55c154de735c0b68f33a95f3c6175bebe2c9c1dc
Code0N <redeemer.of.souls@yandex.ru>
2018-12-13 17:12:15 +0300

    Реализована выгрузка файлов

--- killmeservermain.py
+++ killmeservermain.py
@@ -1,5 +1,12 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from os import curdir, sep
+import os
+import posixpath
+import urllib
+import cgi
+import shutil
+import mimetypes
+import re
+from io import StringIO
 
 from requests import HTTPError
 
@@ -26,18 +33,29 @@ class myHandler(BaseHTTPRequestHandler):
             if self.path.endswith(".css"):
                 mimetype = 'text/css'
                 sendReply = True
+            else:
+                mimetype = 'application/octet-stream'
+                sendReply = True
 
             if sendReply == True:
-                self.send_response(200)
-                self.send_header('Content-type', mimetype)
-                self.end_headers()
-                if mimetype == 'text/html' or mimetype == 'application/javascript' or mimetype == 'text/css':
-                    f = open(curdir + sep + self.path)
-                    self.wfile.write(bytes(f.read(), "utf-8"))
+                if mimetype != "application/octet-stream":
+                    self.send_response(200)
+                    self.send_header('Content-type', mimetype)
+                    self.end_headers()
+                    if mimetype == 'text/html' or mimetype == 'application/javascript' or mimetype == 'text/css':
+                        f = open(os.curdir + os.sep + self.path)
+                        self.wfile.write(bytes(f.read(), "utf-8"))
+                    else:
+                        f = open(os.curdir + os.sep + self.path, 'rb')
+                        self.wfile.write(f.read())
+                    f.close()
                 else:
-                    f = open(curdir + sep + self.path, 'rb')
-                    self.wfile.write(f.read())
-                f.close()
+                    print("Передача файла {} началась\n".format(os.curdir + os.sep + self.path))
+                    with open(os.curdir + os.sep + self.path, 'rb') as fh:
+                        self.send_response(200)
+                        self.send_header('Content-type', 'application/octet-stream')
+                        self.end_headers()
+                        self.wfile.write(fh.read())
             return
 
         except IOError:
@@ -48,6 +66,12 @@ class myHandler(BaseHTTPRequestHandler):
             self.send_error(406)
         except TimeoutError:
             self.send_error(504)
+
+        def do_POST(self):
+            pass # TODO: Заипашить код
+
+        def deal_post_data(self):
+            print(self.hea)
 
 
 try:
